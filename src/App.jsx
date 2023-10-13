cdb1e552110ba032e5b78aad5d957f5bd1d3bc44
R Hema <ramakrishnagarihema@gmail.com>
2023-10-13 17:52:31 +0530

    added form select component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import LoginForm from "./components/login/Login";
 import SignUp from "./components/signup/Signup";
 import About from "./components/lists/about/About";
 import Home from "./components/home/Home";
-
+import FormSelect from "./components/lists/products/FormSelect";
 import "./App.css";
 
 function App() {
@@ -16,6 +16,7 @@ function App() {
         <Route path="login" element={<LoginForm />} />
         <Route path="signup" element={<SignUp />} />
         <Route path="about" element={<About />} />
+        <Route path="/products" element={<FormSelect />}></Route>
       </Routes>
     </div>
   );
