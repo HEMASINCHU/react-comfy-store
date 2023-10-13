a5776a0f926c6ec1a765ed6fb38a288769ff26b0
R Hema <ramakrishnagarihema@gmail.com>
2023-10-13 15:45:02 +0530

    about component added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import LoginForm from "./components/login/Login";
 import SignUp from "./components/signup/Signup";
+import About from "./components/lists/about/About";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Header />} />
         <Route path="login" element={<LoginForm />} />
         <Route path="signup" element={<SignUp />} />
+        <Route path="about" element={<About />} />
       </Routes>
     </div>
   );
