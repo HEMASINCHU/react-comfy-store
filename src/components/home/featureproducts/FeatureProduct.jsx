29f7d06626473cad9a3855098462298e38b99002
R Hema <ramakrishnagarihema@gmail.com>
2023-10-15 16:05:15 +0530

    added css on featureProdutcs

--- src/components/home/featureproducts/FeatureProduct.jsx
+++ src/components/home/featureproducts/FeatureProduct.jsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+import "./featureproduct.css";
 const FeatureProduct = () => {
   return (
     <>
@@ -5,43 +7,49 @@ const FeatureProduct = () => {
       <hr />
       <div className="products">
         <div>
-          <a href="#">
+          <NavLink href="#" className="product-container">
             <img
               src={
                 "https://images.pexels.com/photos/943150/pexels-photo-943150.jpeg?auto=compress&cs=tinysrgb&w=1600"
               }
               alt="lamp"
               loading="lazy"
+              width="200px"
+              height="200px"
             />
-            <p>Avant-Grade-Lamp</p>
+            <h1>Avant-Grade-Lamp</h1>
             <span>$179.99</span>
-          </a>
+          </NavLink>
         </div>
         <div>
-          <a href="#">
+          <NavLink href="#" className="product-container">
             <img
               src={
                 "https://images.pexels.com/photos/3679601/pexels-photo-3679601.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
               }
               alt="table"
               loading="lazy"
+              width="200px"
+              height="200px"
             />
-            <p>Coffee Table</p>
+            <h1>Coffee Table</h1>
             <span>$179.99</span>
-          </a>
+          </NavLink>
         </div>
         <div>
-          <a href="#">
+          <NavLink href="#" className="product-container">
             <img
               src={
                 "https://images.pexels.com/photos/1034584/pexels-photo-1034584.jpeg?auto=compress&cs=tinysrgb&w=1600"
               }
               alt="bed"
               loading="lazy"
+              width="200px"
+              height="200px"
             />
-            <p>Comfy Bed</p>
+            <h1>Comfy Bed</h1>
             <span>$129.99</span>
-          </a>
+          </NavLink>
         </div>
       </div>
     </>
