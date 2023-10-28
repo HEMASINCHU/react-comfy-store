8dca48f9f5d7f46f90f9bff8da834458172625a2
R Hema <ramakrishnagarihema@gmail.com>
2023-10-28 11:36:09 +0530

    Added the previous and next button for product page

--- src/components/lists/products/products/Products.jsx
+++ src/components/lists/products/products/Products.jsx
@@ -4,17 +4,38 @@ import "./products.css";
 
 const Products = () => {
   const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
-    fetch("https://strapi-store-server.onrender.com/api/products")
+    fetch(`https://strapi-store-server.onrender.com/api/products?page=${page}`)
       .then((res) => res.json())
       .then((res) => setData(res.data));
-  }, []);
+  }, [page]);
+
+ const handlePreviousClick = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
+  const handleNextClick = () => {
+    if (data.length > 0) {
+      setPage(page + 1);
+    }
+  };
+
+
+  const isPreviousDisabled = page === 1;
+  const isNextDisabled = data.length === 0;
+
   return (
     <div style={{ margin: "4rem" }}>
       <div className="views">
-        <button onClick="listView()">List</button>
-        <button onClick="gridView()">Grid</button>
+        <button onClick={handlePreviousClick} disabled={isPreviousDisabled}>
+          Grid
+        </button>
+        <button onClick={handleNextClick} disabled={isNextDisabled}>
+          Flex
+        </button>
       </div>
       <hr />
       <div className="products">
@@ -37,7 +58,16 @@ const Products = () => {
             </div>
           ))}
       </div>
+      <div className="pagination-button">
+        <button className=" btn prev" onClick={handlePreviousClick} disabled={isPreviousDisabled}>
+          Previous
+        </button>
+        <button className="btn next" onClick={handleNextClick} disabled={isNextDisabled}>
+          Next
+        </button>
+      </div>
     </div>
   );
 };
+
 export default Products;
